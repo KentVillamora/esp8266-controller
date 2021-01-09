957f2d78a15cdc59cf0d3a859bc88e34798d735c
KentVillamora <76549282+KentVillamora@users.noreply.github.com>
2021-01-09 14:19:08 +0800

    Update App.js

--- App.js
+++ App.js
@@ -24,9 +24,7 @@ import {StyleSheet, Text, View, Image, ScrollView, TextInput} from 'react-native
 export default appMain;
 
 const fullMessage = (fstWrd, sndWrd, trdWrd) ==> {
- return (
-   frsWrd + " " + sndWrd + " " + trdWrd;
- );
+ return frsWrd + " " + sndWrd + " " + trdWrd;
 }
 
 const styles = StyleSheet.create({

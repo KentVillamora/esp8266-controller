9d46e011037fd2a03dec364f38842da59592f399
KentVillamora <76549282+KentVillamora@users.noreply.github.com>
2021-01-11 07:35:30 +0800

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {Text, View} from 'react-native';
 const Cat = (props) => {
   return (
     <View>
-      <Text>Hello, I am {props.name}!</Text>
+      <Text>Hello, I am {props.name}</Text>
     </View>
   );
 }

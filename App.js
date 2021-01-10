c6c4ac6aa474003300ee7f05efd8fbc78e069223
KentVillamora <76549282+KentVillamora@users.noreply.github.com>
2021-01-11 07:11:55 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,44 +1,19 @@
-import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View, Image, ScrollView, TextInput} from 'react-native';
+import {Text, View} from 'react-native';
 
- const appMain = () => {
-   return (
-    <ScrollView>
-      <Text>Hello World!</Text>
-      <View style = {styles.container}>
-        <Text>A text with style: {fullMessage("Hello", "World", "!")}</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{ width: 200, height: 200 }}
-        />
-      </View>
-      <TextInput style = {styles.inputText}
-        defaultValue="Enter your name here"
-      />
-    </ScrollView>
-  );
-}
-export default appMain;
-
-const fullMessage = (fstWrd, sndWrd, trdWrd) => {
- return (
-  fstWrd + " " + sndWrd + " " + trdWrd
+const greetMsg = () => {
+ return(
+  <Text>Hi, I am {props.name}</Text>
  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputText: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1
-  }
-});
+const appMain = () => {
+ return (
+  <View>
+   <Text>Hello World!</Text>
+   <greetMsg name="Kent"/>
+  </View>
+ );
+}
+
+export default appMain;

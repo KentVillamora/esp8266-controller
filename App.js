93fe047a11865941b1f6aeb1ca887b72fe6d10e9
KentVillamora <76549282+KentVillamora@users.noreply.github.com>
2021-01-11 10:12:38 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,50 +1,17 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-const Infinity = () => {
-  return (
-    <View style={styles.infinity}>
-      <View style={styles.infinityBefore} />
-      <View style={styles.infinityAfter} />
-    </View>
-  );
-};
+const Circle = () => {
+  return <View style={styles.circle} />;
+}
 
-export default Infinity;
+export default Circle;
 
-StyleSheet.create({
-  infinity: {
-    width: 80,
+const styles = StyleSheet.create({
+  circle: {
+    width: 100,
     height: 100,
-  },
-  infinityBefore: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: 0,
-    height: 0,
-    borderWidth: 20,
-    borderColor: "red",
-    borderStyle: "solid",
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
-    borderBottomRightRadius: 50,
-    borderBottomLeftRadius: 0,
-    transform: [{ rotate: "-135deg" }],
-  },
-  infinityAfter: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    width: 0,
-    height: 0,
-    borderWidth: 20,
-    borderColor: "red",
-    borderStyle: "solid",
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 0,
-    borderBottomRightRadius: 50,
-    borderBottomLeftRadius: 50,
-    transform: [{ rotate: "-135deg" }],
+    borderRadius: 100 / 2,
+    backgroundColor: "red",
   },
 });

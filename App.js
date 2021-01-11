383dd9b9dbe97248b6c56640b488f9c44c6cdca2
KentVillamora <76549282+KentVillamora@users.noreply.github.com>
2021-01-11 10:26:20 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-const Circle = () => {
-  return <View style={styles.circle} />;
+const SteerWheel = () => {
+  return (
+    <View style={styles.handRing} />
+    <View style={styles.handSpoke} />
+  );
 }
 
 export default Circle;
 
 const styles = StyleSheet.create({
-  circle: {
+  handRing: {
     width: 100,
     height: 100,
     borderRadius: 50,
     borderWidth: 15,
     backgroundColor: "white",
     borderColor: "red",
+  },
+  handSpoke: {
+    width: 100,
+    height: 30,
+    backgroundColor: "red",
+    position: "absolute",
+    right: 0,
+    top: 50,
   },
 });

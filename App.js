1ddf368626696f88823597c2bd12195f9c4b7642
KentVillamora <76549282+KentVillamora@users.noreply.github.com>
2021-01-11 10:04:18 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,50 @@
-import React, { useState } from 'react'
-import { useGamepads } from 'react-gamepads'
-export default function App() {
-  const [gamepads, setGamepads] = useState({})
-  useGamepads((gamepads) => setGamepads(gamepads))
-  // Shows when the "fourth button" of "first gamepad" is pressed
-  return <div>{gamepads[0].buttons[4].pressed ? 'Pressed' : 'Not Pressed'}</div>
-}
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const Infinity = () => {
+  return (
+    <View style={styles.infinity}>
+      <View style={styles.infinityBefore} />
+      <View style={styles.infinityAfter} />
+    </View>
+  );
+};
+
+export default Infinity;
+
+StyleSheet.create({
+  infinity: {
+    width: 80,
+    height: 100,
+  },
+  infinityBefore: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: 0,
+    height: 0,
+    borderWidth: 20,
+    borderColor: "red",
+    borderStyle: "solid",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+    borderBottomLeftRadius: 0,
+    transform: [{ rotate: "-135deg" }],
+  },
+  infinityAfter: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+    borderWidth: 20,
+    borderColor: "red",
+    borderStyle: "solid",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 50,
+    borderBottomLeftRadius: 50,
+    transform: [{ rotate: "-135deg" }],
+  },
+});

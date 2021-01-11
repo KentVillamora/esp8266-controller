2b69936bac5cab8ce0913805f84d07098c9b44b9
KentVillamora <76549282+KentVillamora@users.noreply.github.com>
2021-01-11 09:56:58 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-
-const appMain=()=>{
-  return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
-  );
+import React, { useState } from 'react'
+import { useGamepads } from 'react-gamepads'
+export default function App() {
+  const [gamepads, setGamepads] = useState({})
+  useGamepads((gamepads) => setGamepads(gamepads))
+  // Shows when the "fourth button" of "first gamepad" is pressed
+  return <div>{gamepads[0].buttons[4].pressed ? 'Pressed' : 'Not Pressed'}</div>
 }
-export default appMain;

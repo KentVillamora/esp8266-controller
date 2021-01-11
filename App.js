891a2a908e6302edf017098a52b026aea2421768
KentVillamora <76549282+KentVillamora@users.noreply.github.com>
2021-01-11 08:35:04 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-
-const GreetMsg = (props) => {
-  return (
-    <Text>Hi, I'm {props.name}</Text>
-  );
-}
+import React, {useState} from 'react';
+import {Text, View, Button} from 'react-native';
 
 const appMain = () => {
+  const [wasPressed,setPressStatus] = useState(false);
+  
   return (
     <View>
-     <GreetMsg name="Kent"/>
+      <Text>Press the button below</Text>
+      <Button
+        title={wasPressed ? "You already pressed the button" : "Still waiting for you to press the button"}
+        onPress={()=>{setPressStatus(true)}}
+        disabled={wasPressed}
+      />
     </View>
   );
 }
-
-export default appMain;
